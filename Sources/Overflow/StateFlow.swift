bbd27b1d78be767fc1a23efae5291c801d504cce
Lukas Simonson <lukas@hydradesignllc.com>
2025-06-10 20:23:05 -0600

    Migrates StateFlow.State
    Migrates the StateFlow.State type to implement the SharedBufferedEmitter protocol.

--- Sources/Overflow/StateFlow.swift
+++ Sources/Overflow/StateFlow.swift
@@ -7,23 +7,7 @@
 
 import Foundation
 
-fileprivate protocol StateFlowProtocol: Sendable, Flow where AsyncIterator == Subscription {
-    typealias State = StateFlow<Element>.State
-    typealias Subscription = StateFlow<Element>.Subscription
-    
-    var state: State { get }
-    var value: Element { get async }
-    
-    init(initial: Element)
-}
-
-extension StateFlowProtocol {    
-    public func makeAsyncIterator() -> Subscription {
-        Subscription(state: state)
-    }
-}
-
-public final class StateFlow<Element: Sendable>: StateFlowProtocol {
+public final class StateFlow<Element: Sendable>: Flow {
     fileprivate let state: State
     
     public var value: Element { get async { await state.value } }
@@ -35,9 +19,16 @@ public final class StateFlow<Element: Sendable>: StateFlowProtocol {
     fileprivate init(state: State) {
         self.state = state
     }
+    
+    public func makeAsyncIterator() -> Subscription {
+        Subscription(state: state)
+    }
 }
 
-public final class MutableStateFlow<Element: Sendable>: MutableFlow, StateFlowProtocol {
+public final class MutableStateFlow<Element: Sendable>: MutableFlow {
+    typealias State = StateFlow<Element>.State
+    public typealias Subscription = StateFlow<Element>.Subscription
+    
     fileprivate let state: State
     
     public var value: Element { get async { await state.value } }
@@ -53,133 +44,54 @@ public final class MutableStateFlow<Element: Sendable>: MutableFlow, StateFlowPr
     public func asStateFlow() -> StateFlow<Element> {
         StateFlow(state: state)
     }
+    
+    public func makeAsyncIterator() -> Subscription {
+        Subscription(state: state)
+    }
 }
 
-// MARK: State
 extension StateFlow {
-    actor State {
+    actor State: SharedBufferedEmitter {
         
-        private let maxBufferSize = 5
-        private var overflowContinuations = [UUID: [CheckedContinuation<Void, Never>]]()
+        var value: Element
         
-        /// The current value.
-        private(set) var value: Element
+        var maxBufferSize: Int = 5
+        var overflowContinuations = [UUID : [CheckedContinuation<Void, Never>]]()
         
-        /// The list of continuations waiting for the next value.
-        private var continuations = [UUID: CheckedContinuation<Element, any Error>]()
+        var waitingSubscriptions = [UUID : CheckedContinuation<Element, any Error>]()
+        var buffers = [UUID : WeakBuffer<Subscription, Element>]()
         
-        /// The buffers of all subscribers.
-        private var buffers = [UUID: WeakBuffer<Subscription, Element>]()
-        
-        /// Initializes with an initial value.
-        /// - Parameter initial: The initial value.
         init(initial: Element) {
             self.value = initial
         }
         
         func register(_ subscription: Subscription) {
             if buffers[subscription.id] == nil {
-                // Create new buffer, with current value.
                 buffers[subscription.id] = WeakBuffer(key: subscription, buffer: [value])
             }
         }
         
-        func cancel(id: UUID) {
-            buffers.removeValue(forKey: id)
-            let cont = continuations.removeValue(forKey: id)
-            cont?.resume(throwing: ContinuationError.continuationOwnerExitedScope)
-        }
-        
-        /// Emits a new value and resumes all waiting subscribers.
-        /// - Parameter newValue: the value to emit
         func emit(_ newValue: Element) async {
             value = newValue
-            
-            for id in buffers.keys {
-                guard var buffer = buffers[id] else { continue }
-                
-                // Remove buffer is Subscription has left scope.
-                if buffer.isCleared {
-                    cancel(id: id)
-                }
-                
-                // Handle any current continuations before adding to buffer.
-                else if let cont = continuations.removeValue(forKey: id) {
-                    // No values should be in buffer; however, if there are, emit the first buffered value, then update the buffer.
-                    if let buffed = buffer.removeFirst() {
-                        cont.resume(returning: buffed)
-                        buffer.append(newValue)
-                        buffers[id] = buffer // Update buffers with updated buffer.
-                    } else {
-                        // Continue the waiting subscription without adding to buffer.
-                        cont.resume(returning: newValue)
-                    }
-                }
-                
-                // If the buffer is needed, add value to the buffer.
-                else {
-                    buffer.append(value)
-                    buffers[id] = buffer // Update buffers with updated buffer.
-                    
-                    // Handle buffer overflow
-                    if buffer.size > maxBufferSize {
-                        await withCheckedContinuation { cont in
-                            overflowContinuations[id, default: []].append(cont)
-                        }
-                    }
-                }
-            }
-        }
-        
-        /// Suspends until the next value is emitted, then returns it.
-        /// - Returns the next emitted value.
-        func awaitNextValue(id: UUID) async throws -> Element {
-            if var buffer = buffers[id], let first = buffer.removeFirst() {
-                buffers[id] = buffer // Update buffer with removed value
-                
-                if var conts = overflowContinuations[id], !conts.isEmpty {
-                    conts.removeFirst().resume()
-                    overflowContinuations[id] = conts.isEmpty ? nil : conts
-                }
-                
-                return first
-            }
-            
-            guard continuations[id] == nil else { fatalError("Continuation not called before being removed") }
-            
-            // No Buffer, await next value
-            return try await withCheckedThrowingContinuation { cont in
-                 continuations[id] = cont
-            }
+            await _emit(newValue)
         }
     }
-}
-
-// MARK: Subscription
-extension StateFlow {
-    public final class Subscription: Sendable, AsyncIteratorProtocol {
-        
-        /// The Unique ID of this Subscription
-        let id = UUID()
-        
-        /// The state actor to observe.
+    
+    public final class Subscription: SharedBufferedSubscription {
+        public let id = UUID()
         private let state: State
         
-        /// Initializes the subscription with a state actor.
-        /// - Parameter state: The state to observe.
         init(state: State) {
             self.state = state
         }
         
-        /// Returns the next value in the sequence, suspending until a new value is available.
         public func next() async -> Element? {
             await state.register(self)
             return try? await state.awaitNextValue(id: id)
         }
         
-        public func cancel() async {
+        func cancel() async {
             await state.cancel(id: id)
         }
     }
 }
-
